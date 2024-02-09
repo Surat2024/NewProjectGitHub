f472cbb7c555af8ca6baafb50250ade220041393
Surat <suratmamedov07@gmail.com>
2024-02-09 13:36:11 +0300

    1: add rows 7 and 8

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
 
+        System.out.println("Новая строка1");
+        System.out.println("Новая строка2");
+
 
     }
 }
